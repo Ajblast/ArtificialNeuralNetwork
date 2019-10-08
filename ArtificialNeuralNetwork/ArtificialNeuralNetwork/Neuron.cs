914ab1b10576b26dc23c5fe060e2fd30c6639fbe
Austin Kincer <austinkincer@gmail.com>
2019-10-08 11:04:54 -0400

    Created ANN

---
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork/Neuron.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArtificialNeuralNetwork
+{
+    public class Neuron
+    {
+        protected List<double> weights;
+        private List<double> inputs;
+
+        private double output;
+        private double preOutput;
+
+        protected double bias = 1;
+
+        private Func<double, double> activationFunction;
+
+        public Neuron(int numberInputs, Func<double, double> activationFunction)
+        {
+            weights = new List<double>(numberInputs + 1);
+            inputs = new List<double>(numberInputs);
+
+            this.activationFunction = activationFunction;
+
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            System.Random random = new Random();
+            for (int i = 0; i < weights.Count; i++)
+            {
+                weights[i] = random.NextDouble();
+            }
+        }
+    }
+}
---
+++ ArtificialNeuralNetwork/Math/Mathf.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Math
+{
+    public static class Mathf
+    {
+        public static float Sigmoid (float value)
+        {
+            return 1f / (1f + MathF.Pow(MathF.E, -value));
+        }
+        public static float Sigmoid(float value, float alpha)
+        {
+            return 1f / (1f + MathF.Pow(MathF.E, alpha * (-value)));
+        }
+
+        public static float HyperbolicTangent(float value)
+        {
+            return (1 - MathF.Pow(MathF.E, -value)) / (1 + MathF.Pow(MathF.E, -value));
+        }
+        public static float HyperbolicTangent(float value, float alpha)
+        {
+            return (1 - MathF.Pow(MathF.E, alpha * (-value))) / (1 + MathF.Pow(MathF.E, alpha * (-value)));
+        }
+
+        public static float HardLimit(float value)
+        {
+            return (value < 0) ? 0 : 1;
+        }
+    }
+}
